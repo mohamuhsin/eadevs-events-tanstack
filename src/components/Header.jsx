d2aa93deccc046d766946ecdfd7a443836db393e
mohamuhsin <muhsinohoho@outlook.com>
2024-10-17 01:36:31 +0300

    tanstack query started

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+export default function Header({ children }) {
+  return (
+    <>
+      <div id="main-header-loading"></div>
+      <header id="main-header">
+        <div id="header-title">
+          <h1>Eadevs Events</h1>
+        </div>
+        <nav>{children}</nav>
+      </header>
+    </>
+  );
+}
